250721689ee9b40fcbe84ed576480ecc7b4b06ac
Maxim Barskiy <mmb2283@columbia.edu>
2016-07-27 14:53:52 -0400

    evenodd file

---
+++ evenodd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int c;
+printf("Enter a number:");
+int x;
+scanf("%d", &x);
+if(x%2==0){
+
+printf("\n even");
+}
+else { 
+	printf("\n odd");}
+return 0;
+
+}
+
